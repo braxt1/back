c6a0571647bb3b5a267e370d0f5ff6d2344efc76
Victor Miene <Viktormiene.vm@gmail.com>
2021-02-21 14:28:15 +0100

    Added a Procfile

---
+++ controllers/adminController.js
@@ -0,0 +1,60 @@
+const db = require('../dbconfig')
+const {isEmail,isEmpty} =  require('validator')
+const Knex = require('knex')
+
+const checkEmail = (email) => {
+    let valid =  true
+    if(isEmpty(email) || !isEmail(email)){
+        valid = false
+    }
+    return valid
+} 
+
+const allUsers = async (req,res) => {
+    const users = await db('users')
+    res.json(users)
+}
+
+const editUser = async (req,res) => {
+    const {email,name,deposit,profits,withdrwal,referral,joined} = req.body
+    
+    console.log('edit emaila',req.body)
+
+    if(checkEmail(email)){
+        try{
+            //returns 1 if done
+        const isDone = await db('users').where({email})
+                        .update({email,name,deposit,profits,withdrwal,referral,joined})
+        res.json(isDone)
+        }
+        catch(err){
+        res.json({err:"try again later?"})
+        }
+    }
+    else{
+        res.json({err: "invalid email"})
+    }
+}
+
+const del = async (req,res) => {
+    const {email} = req.body
+    try{
+        //if not the admin delete
+        isdeleted = await db('users').where({email}).del()
+        if(isdeleted){
+            res.json({msg:"success"})
+        }
+        else{
+            res.json({msg:'failed'})
+        }
+        
+    }
+    catch(err){
+        res.json({msg:"failed"})
+    }
+    
+
+}
+
+
+module.exports = {allUsers,editUser,del}
---
+++ routes/adminRoutes.js
@@ -0,0 +1,14 @@
+const {Router} = require('express')
+const router = Router()
+const {allUsers,editUser,del} = require('../controllers/adminController')
+const {requireA} = require('../middleware/AdminMiddleware')
+
+
+// router.use(requireA)
+router.post('/all', requireA, allUsers)
+
+router.post('/editUser', editUser)
+
+router.post('/delete', del)
+
+module.exports = router
